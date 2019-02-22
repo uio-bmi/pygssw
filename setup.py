bdbcf9eeed80f05a022e7a2c2c94561a7d573099
Ivar Grytten <ivar.grytten@gmail.com>
2019-02-22 11:01:14 +0100

    Working pip package

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 from distutils.core import setup, Extension
 
-gssw_extension = Extension('_gssw', sources=['pygssw/gssw_wrap.c', 'pygssw/gssw.c'], )
+gssw_extension = Extension('_gssw', sources=['pygssw/gssw_wrap.c', 'pygssw/gssw.c'], extra_compile_args=["-msse4"])
 
 
 setup(name='pygssw',
-      version='0.0.1',
+      version='0.0.4',
       description='Wrapper around GSSW, making it possible to align reads to graphs from python using GSSW',
-      url='',
+      url='http://github.com/uio-bmi/pygssw',
       author='Ivar Grytten',
       author_email='ivargry@ifi.uio.no',
+      packages=["pygssw"],
       license='MIT',
       zip_safe=False,
       install_requires=[],
@@ -17,6 +18,6 @@ setup(name='pygssw',
             'Programming Language :: Python :: 3'
       ],
       ext_modules=[gssw_extension],
-      package_data={'': ['pygssw/gssw.h']}
-
+      package_data={'': ['pygssw/gssw.h']},
+      include_package_data=True,
 )
